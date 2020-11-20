3118292934c9a6a32dde97d81e92b0ef8e989977
zhukaijun0629 <kaijun.zhu0629@gmail.com>
2020-11-20 10:22:15 -0500

    Update PAs

--- Course #1/PA #1-3/quickSort_and_Comp_Count.py
+++ Course #1/PA #1-3/quickSort_and_Comp_Count.py
@@ -1,37 +1,72 @@
 def quickSort_First(A):
+    # QuickSort Algorithm
+
+    # Base Case: if length of array A is less than or equal to 1
+    # Return A itself as the sorted array, and 0 as number of comparisons happened
     if len(A) <= 1:
         return A,0
+
+    # General Case: divide and conquer
+    # using the first element in A as the pivot element 'p'
+    # recursively partition array A into two arrays and sort seperately
+    # (1st array B containing all elements smaller than pivot and
+    # 2nd array C containing all elements larget than pivot)
+    # Return the compiled sorted array B+[p]+C, and total count of comparisons happened
     else:
         p = A[0]
-        A,Count1,p1 = partition(A,0,len(A))
+        A,Count1,p1 = partition(A)
         B,Count2 = quickSort_First(A[:p1])
         C,Count3 = quickSort_First(A[p1+1:])
         return B+[p]+C , Count1+Count2+Count3
 
 def quickSort_Last(A):
+    # QuickSort Algorithm
+
+    # Base Case: if length of array A is less than or equal to 1
+    # Return A itself as the sorted array, and 0 as number of comparisons happened
     if len(A) <= 1:
        return A,0
+
+    # General Case: divide and conquer
+    # using the last element in A as the pivot element 'p'
+    # recursively partition array A into two arrays and sort seperately
+    # (1st array B containing all elements smaller than pivot and
+    # 2nd array C containing all elements larget than pivot)
+    # Return the compiled sorted array B+[p]+C, and total count of comparisons happened
     else:
        p = A[-1]
        A[0],A[-1] = A[-1],A[0]
-       A,Count1,p1 = partition(A,0,len(A))
+       A,Count1,p1 = partition(A)
        B,Count2 = quickSort_Last(A[:p1])
        C,Count3 = quickSort_Last(A[p1+1:])
        return B+[p]+C , Count1+Count2+Count3
 
-def quickSort_Medium(A):
+def quickSort_Middle(A):
+    # QuickSort Algorithm
+
+    # Base Case: if length of array A is less than or equal to 1
+    # Return A itself as the sorted array, and 0 as number of comparisons happened
     if len(A) <= 1:
        return A,0
+
+    # General Case: divide and conquer
+    # using the Middle element as the pivot element 'p'
+    # (Refer to README for the definition of the Middle element)
+    # recursively partition array A into two arrays and sort seperately
+    # (1st array B containing all elements smaller than pivot and
+    # 2nd array C containing all elements larget than pivot)
+    # Return the compiled sorted array B+[p]+C, and total count of comparisons happened
     else:
-        p0 = get_Medium_Index(A)
+        p0 = get_Middle_Index(A)
         p = A[p0]
         A[0],A[p0] = A[p0],A[0]
-        A,Count1,p1 = partition(A,0,len(A))
-        B,Count2 = quickSort_Medium(A[:p1])
-        C,Count3 = quickSort_Medium(A[p1+1:])
+        A,Count1,p1 = partition(A)
+        B,Count2 = quickSort_Middle(A[:p1])
+        C,Count3 = quickSort_Middle(A[p1+1:])
         return B+[p]+C , Count1+Count2+Count3
 
-def get_Medium_Index(A):
+def get_Middle_Index(A):
+    # To get the index of the Middle element in array A
     if (A[0]-A[(len(A)-1)//2]) * (A[0]-A[(len(A)-1)]) < 0:
         return 0
     elif (A[(len(A)-1)//2]-A[0]) * (A[(len(A)-1)//2]-A[(len(A)-1)]) < 0:
@@ -39,10 +74,13 @@ def get_Medium_Index(A):
     else:
         return len(A)-1
 
-
-def partition(A,l,r):
+def partition(A):
+    # To partition the array A using the first element 'p' as pivot element
+    # Return the sorted array, Count of Comparisons happend, and index of 'p' in the sorted array
     Count = 0
-    p = A[l]
+    p = A[0]
+    l = 0
+    r = len(A)
     i = l + 1
     for j in range(l+1,r):
         if A[j] < p:
@@ -64,4 +102,4 @@ import copy
 
 print(quickSort_First(copy.deepcopy(A))[1]) # 162085
 print(quickSort_Last(copy.deepcopy(A))[1]) # 164123
-print(quickSort_Medium(copy.deepcopy(A))[1]) # 138382
+print(quickSort_Middle(copy.deepcopy(A))[1]) # 138382
--- Course #1/PA #1-4/kargerMinCut.py
+++ Course #1/PA #1-4/kargerMinCut.py
@@ -8,11 +8,11 @@ def choose_random_key(G):
 
 def kargerMinCut(file_path):
     ###Load Data###
-    data=open(file_path,'r')
-    G={}
+    data = open(file_path,'r')
+    G = {}
     for line in data:
-        lst=[int(s) for s in line.split()]
-        G[lst[0]]=lst[1:]
+        lst = [int(s) for s in line.split()]
+        G[lst[0]] = lst[1:]
 
     # Shrink the graph G by one randomly picked edge (u,v) at a time ...
     # until there is only one edge left
@@ -26,18 +26,18 @@ def kargerMinCut(file_path):
             G[w].remove(v) # To v's neighbors, update v to u as the neighbor
             G[w].append(u)
         while u in G[u]:
-            G[u].remove(u)# remove cycles
+            G[u].remove(u) # remove cycles
         del G[v]
     return len(G[u])
 
 def operation(n):
     # repeat the Karger Minimum Cut operation n times to get the Global Minimum
     # a single operation won't necessarily get to the Global Minimum due to random selection
-    i=0
+    i = 0
     mincut = float('inf')
-    while i<n:
-        mincut=min(mincut,kargerMinCut("kargerMinCut.txt"))
-        i+=1
+    while i < n:
+        mincut = min(mincut,kargerMinCut("kargerMinCut.txt"))
+        i += 1
         # print(mincut) # Uncomment this line to view the minimum cut progress
     return mincut
 
