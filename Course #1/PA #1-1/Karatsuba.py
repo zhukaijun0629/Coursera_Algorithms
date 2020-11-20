4537ae81976d6ffc0e8683e57b6a20f2e0e9d182
zhukaijun0629 <kaijun.zhu0629@gmail.com>
2020-11-19 22:34:14 -0500

    Updated PA 1-1 and 1-2

--- Course #1/PA #1-1/Karatsuba.py
+++ Course #1/PA #1-1/Karatsuba.py
@@ -1,20 +1,32 @@
 def karatsuba(x,y):
-    if x<10 and y<10:
-        return x*y
+    # Base Case: if both x and y are single digit integers
+    # Then return the multilication product
+    if x < 10 and y < 10:
+        return x * y
+
+    # General Case: divide and conquer
+    # Recursively split x into a, b and y into c, d
+    # e.g. 1st step   x = 1234 -> a = 12, b = 34
+    #                 y = 567  -> c = 5,  d = 67
+    # Then returns (a*c)*10000 + (a*d+b*c)*100 + b*d
+    # The above step will be called recursively until it hits the Base Case
     else:
-        n=max(len(str(x)),len(str(y)))
+        n = max( len(str(x)) , len(str(y)) ) // 2 #split longer length in half
+        digits = 10**(n)
 
-        a=x//(10**(n//2))
-        b=x%(10**(n//2))
-        c=y//(10**(n//2))
-        d=y%(10**(n//2))
+        a = x // digits
+        b = x % digits
+        c = y // digits
+        d = y % digits
 
-        ac=karatsuba(a,c)
-        bd=karatsuba(b,d)
-        abcd=karatsuba(a+b,c+d)-ac-bd
+        ac = karatsuba(a,c)
+        bd = karatsuba(b,d)
+        ad_bc = karatsuba(a + b,c + d) - ac - bd
+        # a*d+b*c = (a+b)*(c+d) - a*c - b*d
 
-        return ac*(10**(2*(n//2)))+abcd*(10**(n//2))+bd
-Results=karatsuba(3141592653589793238462643383279502884197169399375105820974944592\
+        return ac*(digits**2) + ad_bc*(digits) + bd
+
+results=karatsuba(3141592653589793238462643383279502884197169399375105820974944592\
             ,2718281828459045235360287471352662497757247093699959574966967627)
-print(Results)
+print(results)
 #8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184
--- Course #1/PA #1-2/sort_and_count_inverstions.py
+++ Course #1/PA #1-2/sort_and_count_inverstions.py
@@ -1,32 +1,42 @@
-def Sort_and_CountInv(A):
-    if len(A)==1:
+def sort_and_CountInv(A):
+    # Base Case: if length of array A is 1, then no need to sort
+    # Return A itself as the sorted array and 0 as the number of inversions happened
+    if len(A) == 1:
         return A,0
+
+    # Genearl Case: divide and conquer
+    # Recursively split array A evenly into two arrays and sort separately
+    # 1st half yields B as the sorted array and X as the number of inversions happened
+    # 2nd half yields C as the sorted array and Y as the number of inversions happened
+    # Then merge B&C to D as the final sorted array and Z as the number of inversions happened
     else:
-        m=len(A)//2
-        B,X=Sort_and_CountInv(A[:m])
-        C,Y=Sort_and_CountInv(A[m:])
-        D,Z=Merge_and_CountSplitInv(B,C)
-    return D,X+Y+Z
+        m = len(A) // 2
+        B,X = sort_and_CountInv(A[:m])
+        C,Y = sort_and_CountInv(A[m:])
+        D,Z = merge_and_CountSplitInv(B,C)
+    return D , X + Y + Z
 
 
-def Merge_and_CountSplitInv(B,C):
-    D=[]
-    Z=0
+def merge_and_CountSplitInv(B,C):
+    # Merge two halves of the sorted arrays B and C
+    # Everytime when the first number of C >= first number of B
+    # THe number of inversions needs to add the length of the remaining array B
+    D = []
+    Z = 0
     while B and C:
-        if B[0]<C[0]:
+        if B[0] < C[0]:
             D.append(B.pop(0))
         else:
             D.append(C.pop(0))
-            Z+=len(B)
+            Z += len(B)
     D.extend(B or C)
     return D,Z
 
 #A=[3,1,6,5,4,2]
-A=[]
-with open ("ints.txt") as file:
+A = []
+with open ("IntegerArray.txt") as file:
     for line in file:
         A.append(int(line))
 
-
-
-print(Sort_and_CountInv(A)[1])
+print(sort_and_CountInv(A)[1])
+# 2407905288
